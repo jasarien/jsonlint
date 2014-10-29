6b0ed762ed8eb6f9d1b440cfb061663d10fb5b3c
James Addyman <addyman@me.com>
2014-10-29 10:28:05 +0000

    Pretty print JSON to stdout using NSJSONSerialization

--- jsonlint/main.m
+++ jsonlint/main.m
@@ -10,8 +10,46 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+//        NSLog(@"argc: %d, argv: %@", argc, [NSString stringWithUTF8String:*argv]);
+        
+        if (argc != 2)
+        {
+            fprintf(stderr, "Usage: %s </path/to/json/file>\n", argv[0]);
+            return 0;
+        }
+        
+        NSString *filepath = [NSString stringWithUTF8String:argv[1]];
+        if (![[NSFileManager defaultManager] fileExistsAtPath:filepath])
+        {
+            fprintf(stderr, "%s no such file\n", argv[1]);
+            return 0;
+        }
+        
+        NSData *jsonData = [NSData dataWithContentsOfFile:filepath];
+        if (!jsonData)
+        {
+            fprintf(stderr, "Unable to read file at %s\n", argv[1]);
+            return 0;
+        }
+        
+        NSError *error = nil;
+        id jsonObject = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+        if (!jsonObject && error)
+        {
+            fprintf(stderr, "Error reading json: %s\n", [[error localizedDescription] UTF8String]);
+            return 0;
+        }
+        
+        error = nil;
+        NSData *prettyJSONData = [NSJSONSerialization dataWithJSONObject:jsonObject options:NSJSONWritingPrettyPrinted error:&error];
+        if (!prettyJSONData && error)
+        {
+            fprintf(stderr, "Error writing json: %s\n", [[error localizedDescription] UTF8String]);
+            return 0;
+        }
+        
+        fprintf(stdout, "%s\n\r", [[[NSString alloc] initWithData:prettyJSONData encoding:NSUTF8StringEncoding] UTF8String]);
     }
     return 0;
 }

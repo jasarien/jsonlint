1769ada25326c0ec0e0bda5c459fbabc8164f046
James Addyman <addyman@me.com>
2014-10-29 09:56:58 +0000

    Initial Commit

---
+++ jsonlint/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  jsonlint
+//
+//  Created by James Addyman on 29/10/2014.
+//  Copyright (c) 2014 Jamsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
